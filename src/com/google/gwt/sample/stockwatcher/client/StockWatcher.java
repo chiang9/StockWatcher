b4e9e789ff9a66767c7a671f5d85da56916220a0
anastasiahu <anastasiahu@dhcp-206-87-195-35.ubcsecure.wireless.ubc.ca>
2015-09-30 19:54:34 -0700

    j

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -57,7 +57,9 @@ public class StockWatcher implements EntryPoint {
 				if (loginInfo.isLoggedIn()) {
 					loadStockWatcher();
 				} else {
-					loadLogin();
+					/**
+					 * NOW I MAKE SOME CHANGE HERE
+					 */
 				}
 			}
 		});
