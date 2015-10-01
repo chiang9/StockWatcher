777066a8c5a0f63f4d968246025e22bf718df56f
anastasiahu <anastasiahu@dhcp-206-87-195-35.ubcsecure.wireless.ubc.ca>
2015-09-30 20:05:07 -0700

    changed

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -46,7 +46,7 @@ public class StockWatcher implements EntryPoint {
 	 * Entry point method.
 	 */
 	public void onModuleLoad() {
-		// Check login status using login service.
+		// Check login status using login servicea.
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
 		loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
 			public void onFailure(Throwable error) {
